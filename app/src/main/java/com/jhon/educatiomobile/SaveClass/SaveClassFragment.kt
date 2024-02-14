762953ac010c96c4ebfe0c0bc5434a95e9291c27
jhon rivera <jhonrivera416@gmail.com>
2024-02-14 15:00:04 -0500

    estructure

---
+++ app/src/main/java/com/jhon/educatiomobile/SaveClass/SaveClassFragment.kt
@@ -0,0 +1,16 @@
+package com.jhon.educatiomobile.SaveClass
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SaveClassFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SaveClassFragment : Fragment() {
+}
---
+++ app/src/main/java/com/jhon/educatiomobile/VerifyTeacher/VerifyTeacherFragment.kt
@@ -0,0 +1,12 @@
+package com.jhon.educatiomobile.VerifyTeacher
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class VerifyTeacherFragment : Fragment() {
+
+}
